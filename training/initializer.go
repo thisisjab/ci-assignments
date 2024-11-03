3668537d5d27e8ec5262c6b1bdb342fa219286e2
thisisjab <heythisisjab@gmail.com>
2024-11-03 22:00:53 +0330

    feat: add data initializer

--- training/initializer.go
+++ training/initializer.go
@@ -4,73 +4,31 @@ import (
 	"log"
 	"xo-detection/data"
 	"xo-detection/models"
-	"xo-detection/training/perceptron"
 	"xo-detection/utils"
 )
 
-func initPerceptronWeights(path string) (calculatedWeights models.SavedWeightAndBiasJsonObject, calculated bool) {
-	if utils.FileExists(path) {
-		log.Println("Loading perceptron weights from " + path)
-		loadedData := data.UnmarshalWeightsFile(path)
-		log.Printf(
-			"Tooks %v epoches for %v to get trained with %v data and theta=%v and learning rate=%v.\n",
-			loadedData.TotalEpoches, "xo-perceptron", loadedData.TrainingDataSize, loadedData.Theta, loadedData.LearningRate)
-		return loadedData, false
+func LoadWeightsOrTrain(savedWeightsPath string, trainingFunc func() models.SavedWeightAndBiasJsonObject) models.SavedWeightAndBiasJsonObject {
+	if !utils.FileExists(savedWeightsPath) {
+		log.Printf("Path to saved weights does not exist: %v\n", savedWeightsPath)
+		log.Printf("Training right now.\n")
+
+		objectToSave := trainingFunc()
+
+		log.Printf("Seems like training is finished: %v\n", objectToSave)
+
+		log.Printf("Saving weights to %v...\n", savedWeightsPath)
+		success, err := data.SaveWeights(objectToSave, savedWeightsPath)
+
+		if !success {
+			log.Printf("Saving data faild.\n")
+			log.Fatalln(err)
+		}
+
+		log.Printf("Saving weights to %v was successful.\n", savedWeightsPath)
+
+		return objectToSave
 	}
 
-	log.Println("File for perceptron weights does not exist. Training the network...")
-	log.Println("Initializing initial weights with 0.")
-	var weights []models.Weights
-	var firstRowWeights models.Weights
-	for i := 0; i < 25; i++ {
-		firstRowWeights = append(firstRowWeights, 0)
-	}
-
-	weights = append(weights, firstRowWeights)
-
-	var bias float64
-
-	const theta = 0.2
-	const learningRate = 0.3
-
-	log.Printf("Using %v as theta and %v as learning rate.\n", theta, learningRate)
-
-	loadedTrainingData := data.UnmarshalTrainingDataFile("./training/data/xo.json")
-	log.Printf("Data for xo is read from %v.\n", path)
-
-	cleanedData := data.PrepareData(loadedTrainingData)
-	log.Printf("Data for xo is cleaned from %v.\n", path)
-	log.Printf("Loaded %v vectors as training data.\n", len(loadedTrainingData))
-
-	totalEpochs := perceptron.Train(cleanedData, &weights, &bias, theta, learningRate)
-
-	result := models.SavedWeightAndBiasJsonObject{Weights: weights[len(weights)-1],
-		Key:              "xo-perceptron",
-		TotalEpoches:     totalEpochs,
-		Bias:             bias,
-		Theta:            theta,
-		LearningRate:     learningRate,
-		TrainingDataSize: len(cleanedData),
-	}
-
-	log.Printf(
-		"Tooks %v epoches for %v to get trained with %v data and theta=%v and learning rate=%v.\n",
-		result.TotalEpoches, "xo-perceptron", result.TrainingDataSize, result.Theta, result.LearningRate)
-
-	success, err := data.SaveWeights(result, path)
-
-	if !success {
-		log.Fatalln(err)
-	}
-
-	return result, true
-}
-
-func GetOrCalculateWeights(key, path string) (weightsAndBias models.SavedWeightAndBiasJsonObject, calculated bool) {
-	switch key {
-	case "xo-perceptron":
-		return initPerceptronWeights(path)
-	default:
-		panic("Unhandled key for `GetOrCalculateWeights`")
-	}
+	log.Printf("Loading weights from %v...\n", savedWeightsPath)
+	return data.UnmarshalWeightsFile(savedWeightsPath)
 }
